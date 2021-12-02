a034fc9099ba068249d3a82ba56530581d1af87d
jester <jaimebuddy98@gmail.com>
2021-12-02 13:37:45 -0600

    further developed alerts and set a conditional control flow in the generator function

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,7 @@ var generateBtn = document.querySelector("#generate");
   // 2. validate the input 
   // 3. generate and display working password
 
+// the source for the password generator 
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
 var numbers = "0123456789";
@@ -17,11 +18,12 @@ var specialCharecters = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
 var passwordLength;
 var chooseLowercase;
 var chooseUppercase;
+var chooseNumber;
 var chooseSymbol; 
 
-
+// function that prompts user to input desired length of password b/w 8 and 128 characters 
 function promptLength () {
-  prompt("What is your desired password length? Please choose a number between 8-128 characters.")
+  passwordLength = prompt("What is your desired password length? Please choose a number between 8-128 characters.")
   if (passwordLength<8){
     alert("Password length has to be a number between 8-128 characters");
     promptLength();
@@ -34,27 +36,68 @@ function promptLength () {
   return passwordLength;
 }
 
+// alert to ask if user would like to include lowercase letters
 function promptLowercase () {
-  chooseLowercase = confirm ("Would you like to include lowercase?");
+  chooseLowercase = confirm ("Would you like to include lowercase letters?");
 }
 
+// alert to ask if user would like to include upppercase letters
 function promptUppercase() {
-  chooseUppercase = confirm ("Would you like to include lowercase?")
+  chooseUppercase = confirm ("Would you like to include uppercase letters?");
 }
 
+// alert to ask if user would like to include upppercase letters
+function promptNumber() {
+  chooseNumber = confirm ("Would you like to include numbers?");
+}
+
+// alert to ask if user would like to include symbols 
 function promptSymbol () {
-  chooseSymbol= confirm ("Would you like to include symbols?")
+  chooseSymbol= confirm ("Would you like to include symbols?");
 }
 
 
 
 function generatePassword() {
   // console.log ('clicked')  // checing button worked 
-  // call my prompts
+  // call my prompts & validate inpute (?)
   promptLength();
+  console.log(passwordLength);
   promptLowercase();
-  promptUppercase()
+  console.log(chooseLowercase);
+  promptUppercase();
+  console.log(chooseUppercase);
+  promptNumber();
+  console.log(chooseNumber)
   promptSymbol();
+  console.log(chooseSymbol);
+
+  if (chooseLowercase && chooseUppercase && chooseSymbol) {
+
+  }
+  else if (chooseLowercase && chooseUppercase) {
+
+  }
+  else if (chooseLowercase && chooseSymbol) {
+
+  }
+  else if (chooseUppercase && chooseSymbol) {
+
+  }
+  else if (chooseLowercase) {
+
+  }
+  else if (chooseUppercase) {
+
+  }
+  else if (chooseSymbol) {
+
+  }
+  else {
+    alert('You must choose one type of character for your password');
+    generatePassword ();
+  }
+
 }
 
 
